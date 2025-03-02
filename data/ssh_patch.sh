51ff6728a8962f757f2e1c4cda23e0927ea2f770
remittor <remittor@gmail.com>
2025-03-02 19:26:09 +0300

    ssh: Cleanup permanent ssh patch

--- data/ssh_patch.sh
+++ data/ssh_patch.sh
@@ -2,12 +2,10 @@
 
 [ -e "/tmp/ssh_patch.log" ] && return 0
 
-HAVE_PATCH=$( grep '= "release"' /etc/init.d/dropbear )
-if [ -z "$HAVE_PATCH" ]; then
-	return 0
+if grep -q '= "release"' /etc/init.d/dropbear ; then
+	sed -i 's/nvram get ssh_en/echo 1/g' /etc/init.d/dropbear
+	sed -i 's/= "release"/= "XXXXXX"/g'  /etc/init.d/dropbear
 fi
-
-sed -i 's/= "release"/= "XXXXXX"/g' /etc/init.d/dropbear
 
 /etc/init.d/dropbear enable
 /etc/init.d/dropbear restart
--- data/ssh_uninstall.sh
+++ data/ssh_uninstall.sh
@@ -1,9 +1,16 @@
 #!/bin/sh
 
+DIR_PATCH=/etc/crontabs/patches
+
+# remove older version of patch
 grep -v "/ssh_patch.sh" /etc/crontabs/root > /etc/crontabs/root.new
 mv /etc/crontabs/root.new /etc/crontabs/root
 /etc/init.d/cron restart
 
-rm -f /etc/crontabs/patches/ssh_patch.sh
+# remove newest version
+uci delete firewall.auto_ssh_patch
+uci commit firewall
+
+rm -f $DIR_PATCH/ssh_patch.sh
 rm -f /tmp/ssh_patch.log
 
